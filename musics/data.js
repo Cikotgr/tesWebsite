934ad933cf7c2b2a249a084f804bae6b2851a2bb
Cikotgr <88586776+Cikotgr@users.noreply.github.com>
2022-03-09 22:05:08 +0700

    Update data.js

--- musics/data.js
+++ musics/data.js
@@ -1,26 +1,26 @@
 let songs = [
     {
         name: 'Puisi Alam',
-        path: '../musics/puisi-alam.mp3',
+        path: 'puisi-alam.mp3',
         artist: 'Fourtwnty',
-        cover: '../musics/img/puisi alam.png'
+        cover: 'img/puisi alam.png'
     },
     {
         name: 'Kusut',
-        path: '../musics/kusut.mp3',
+        path: 'kusut.mp3',
         artist: 'Fourtwnty',
-        cover: '../musics/img/kusut.png'
+        cover: 'img/kusut.png'
     },
     {
         name: 'Pilu Membiru',
-        path: '../musics/pilu-membiru.mp3',
+        path: 'pilu-membiru.mp3',
         artist: 'Kunto Aji',
-        cover: '../musics/img/pilu membiru.png'
+        cover: 'img/pilu membiru.png'
     },
     {
         name: 'Rehat',
-        path: '../musics/rehat.mp3',
+        path: 'rehat.mp3',
         artist: 'Kunto Aji',
-        cover: '../musics/img/rehat.png'
+        cover: 'img/rehat.png'
     }
 ]
